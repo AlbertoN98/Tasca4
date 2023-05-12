cc7a40d280086bbd167a3b466ba81ac76b3dbdf4
AlbertoN98 <albniered@alu.edu.gva.es>
2023-05-12 10:41:41 +0200

    TASCA4 cambiado switch por bucle

--- src/Main.java
+++ src/Main.java
@@ -5,12 +5,20 @@ public class Main {
         System.out.println(Main.gd(0));
 
     }
-    public static String gd(int code){
-        return switch (code) {
-            case 702, 1082, 1083, 1114, 1184, 1266, 12403 -> "DATE";
-            case 20, 21, 23, 24, 26, 700, 701, 790, 1700, 2202, 2203, 2204, 2205, 2206, 3734, 3769, 12396 -> "NUMERIC";
-            default -> "STRING";
-        };
+
+    public static String gd(int code) {
+        String cod = "";
+        int[] num = {702, 1082, 1083, 1114, 1184, 1266, 12403, 20, 21,
+                23, 24, 26, 700, 701, 790, 1700, 2202, 2203, 2204, 2205, 2206, 3734, 3769, 12396};
+
+        for (int i = 0; i < num.length; i++) {
+             switch (code) {
+                case 702, 1082, 1083, 1114, 1184, 1266, 12403 -> cod = "DATE";
+                case  20, 21, 23, 24, 26, 700, 701, 790, 1700, 2202, 2203, 2204, 2205, 2206, 3734, 3769, 12396 -> cod = "NUMERIC";
+                default -> cod = "String";
+            };
+        }
+        return cod;
     }
 }
 
